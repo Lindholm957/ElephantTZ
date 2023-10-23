d89a55ddee2d4d82ac8f167f8f166f55fcb8978b
lindholm <yan957@gmail.com>
2023-10-23 22:55:09 +0300

    added script for rectangle actions

---
+++ Assets/Scripts/RectangleController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RectangleController : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D _collider;
+    [SerializeField] private UnityEvent _action;
+
+    private void OnValidate()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        _action.Invoke();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown (0) | Input.touchCount == 1) {
+            Vector2 pos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pos), Vector2.zero);
+            if (hitInfo.transform.gameObject == _collider.gameObject)
+            {
+                _action.Invoke();
+            }
+        }
+    }
+}
